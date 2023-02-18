2b58f485ca0bd23b334f85562fee2eb004054013
PedroEmanuelMoreiraCarvalho <emanuelpedro381@gmail.com>
2023-02-18 10:37:45 -0300

    test ajusts

--- src/__test__/user.test.ts
+++ src/__test__/user.test.ts
@@ -1,6 +1,5 @@
 import { User } from "../user_controller/user";
 import axios from "axios";
-import { getQuote } from "../services/exchange";
 
 jest.mock('axios');
 
@@ -36,25 +35,5 @@ describe("testing user methods", ()=>{
         expect(user.cash["USD"]).toEqual(19.20);
     });
 });
-
-describe("testing services", ()=>{
-  const data = {
-    "Meta Data": {
-      "4. Last Refreshed": "2023-02-17 11:55:00"
-    },
-    "Time Series FX (5min)": {
-      "2023-02-17 11:55:00": {
-        "1. open": "0.1920"
-      }
-    }
-  };
-  const resp = {data: data};
-  mockedAxios.request.mockResolvedValueOnce(resp);
-
-  it("getQuote should work",async()=>{
-    let quote = await getQuote("BRL","USD");
-    expect(quote).toEqual(0.192);
-  });
-})
 
 jest.clearAllMocks();
